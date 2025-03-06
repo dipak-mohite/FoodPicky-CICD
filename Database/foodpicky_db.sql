5eb3f15dfbca1366c43475b6eb591d727704b43f
Dipak Mohite <123377589+dipak-mohite@users.noreply.github.com>
2025-03-06 20:45:36 +0530

    Update foodpicky_db.sql

--- Database/foodpicky_db.sql
+++ Database/foodpicky_db.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 24, 2022 at 10:37 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
